caa5793d5341eb54e2bf11475ac3e92cf0841df2
Salis Alvarez <saalsa@jviladoms.cat>
2026-09-24 10:56:18 +0200

    24/09/2026

--- ex06.php/index.php
+++ ex06.php/index.php
@@ -1,26 +1,31 @@
 <?php 
-        
+    
+    const IVA = 0.21;
+    const BOTIGA = 'Peru';
+    const MONEDA = '€';
+    const DESCOMPTE_SOCI = 0.10;
+    
     $nomProducte = "Camiseta Maduro";
     $descripcio = "Prenda deportiva ligera y transpirable, con botones delanteros y diseño cómodo, ideal para jugar o vestir de forma casual.";
 
     $preu = 99.99;
-    $iva = 0.21;
     $stock = 10;
     $ref = "CAM-1425376";
 
-    $importIva = $preu * $iva;
+    $importIva = $preu * IVA;
     $total = $preu + $importIva;
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda Online</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.c">
 </head>
 <body>
+
     <header>
         <h1>Tienda Online</h1>
         <p>Esto es una tienda online</p>
@@ -31,13 +36,28 @@
 
             <h2>Camiseta</h2>
 
-            <p class="descripcio">  <?= $nomProducte ?> </p>
-            <p class="preu">Preu sense IVA <?= $preu; ?> EUR </p>
-            <p class="preuIva"> IVA (21%) <?= $importIva; ?> EUR</p>
-            <p class="total">Total: <?= $total; ?> EUR </p>
+            <p class="descripcio"><?= $nomProducte ?></p>
 
-            <p class="estoc"> Stock Disponible: <?= $stock ?></p>
-            <p class="ref"> Referencia: <?= $ref ?></p>
+            <p class="preu">
+                Preu sense IVA: <?= $preu ?> <?= MONEDA ?>
+            </p>
+
+            <p class="preuIva">
+                IVA (<?= IVA * 100 ?>%): <?= $importIva ?> <?= MONEDA ?>
+            </p>
+
+            <p class="total">
+                Total: <?= $total ?> <?= MONEDA ?>
+            </p>
+
+            <p class="estoc">
+                Stock Disponible: <?= $stock ?>
+            </p>
+
+            <p class="ref">
+                Referencia: <?= $ref ?>
+            </p>
+
         </article>
     </main>
 
